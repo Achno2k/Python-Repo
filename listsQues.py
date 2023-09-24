45077a29389bb1345387d609923f6833d2465e41
Achno2k <amansinghh525@gmail.com>
2023-09-24 22:11:08 +0530

    Changed listsQues and added tupleQues

--- listsQues.py
+++ listsQues.py
@@ -1,3 +1,12 @@
+# #Write a Python program to sum all the items in a list
+# lst = [23, 56, 1, 90, 123, 42]
+# max = lst[0]
+# for x in lst:
+#     if max < x:
+#         max = x
+# print("Maximum number in the list is: ", max)
+
+
 # #Write a Python program to get the largest number from a list.
 # lst = [12, 34, 65, 2, 11]
 # max = lst[0]
@@ -5,6 +14,7 @@
 #     if max < x:
 #         max = x
 # print("The maximum element in the list is: ", max)
+
 
 # #Write a Python program to count the number of strings from a given list of strings. The string length is 2 or more and the first and last characters are the same.
 # lst = ['abc', 'xyz', 'aba', '1221']
@@ -14,6 +24,16 @@
 #         count = count + 1
 # print("Result:", count)
 
+
+# #Write a Python program to remove duplicates from a list
+# lst = [1,1,2,2,2,3,4,4,5,5,5,5]
+# newlst = []
+# for x in lst:
+#     if x not in newlst:
+#         newlst.append(x)
+# print(newlst)
+
+
 # # Write a Python program to print the numbers of a specified list after removing even numbers from it.
 # lst = [12, 33, 49, 122, 56, 73]
 # newlst = []            
@@ -21,3 +41,4 @@
 #     if i % 2 != 0:
 #         newlst.append(i)
 # print(newlst)
+
